bb1538769c8371275028bf2ef89d3792d15c04f0
likhit <mrlikhit1992@gmail.com>
2023-08-17 15:12:17 +0700

    update jest config

---
+++ src/app/pass_checker/PasswordChecker.ts
@@ -0,0 +1,36 @@
+/**
+ *  Requirement ???
+ *  lenght is less than 8 charactor
+ *  has no upper case letter
+ *  has no lower case letter
+ */
+
+export enum PasswordErrors {
+  SHORT = "Password is too short!",
+  NO_UPPER_CASE = "Upper case letter required!",
+  NO_LOWER_CASE = "Lower case letter required!",
+}
+
+export interface CheckResult {
+  valid: boolean;
+  reasons: string[];
+}
+
+export class PasswordChecker {
+  public checkPassword(password: string): CheckResult {
+    const reasons: PasswordErrors[] = [];
+    if (password.length < 8) {
+      reasons.push(PasswordErrors.SHORT);
+    }
+    if (password == password.toLowerCase()) {
+      reasons.push(PasswordErrors.NO_UPPER_CASE);
+    }
+    if (password == password.toUpperCase()) {
+      reasons.push(PasswordErrors.NO_LOWER_CASE);
+    }
+    return {
+      valid: reasons.length > 0 ? false : true,
+      reasons: reasons,
+    };
+  }
+}
